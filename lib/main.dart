822e85e769accce8fb7f47a398ebfd82ecbecae3
izaanjahangir <izaanjahangir2@gmail.com>
2021-07-03 02:21:41 +0500

    added user register

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,48 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_status_screen/screens/home/home.dart';
 import 'package:whatsapp_status_screen/screens/login/login.dart';
 import 'package:whatsapp_status_screen/screens/register/register.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Whatsapp status screen',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: "/login",
-      routes: {
-        "/login": (BuildContext context) => Login(),
-        "/register": (BuildContext context) => Register(),
-        "/": (BuildContext context) => Home(),
-      },
-    );
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Container();
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              title: 'Whatsapp status screen',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              initialRoute: "/login",
+              builder: EasyLoading.init(),
+              routes: {
+                "/login": (BuildContext context) => Login(),
+                "/register": (BuildContext context) => Register(),
+                "/": (BuildContext context) => Home(),
+              },
+            );
+          }
+
+          return Container();
+        });
   }
 }
--- lib/screens/register/register.dart
+++ lib/screens/register/register.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
 import 'package:whatsapp_status_screen/config/theme_colors.dart';
 
 class Register extends StatelessWidget {
@@ -6,10 +10,35 @@ class Register extends StatelessWidget {
   Widget build(BuildContext context) {
     final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
     final TextEditingController emailController = TextEditingController();
+    final TextEditingController fullNameController = TextEditingController();
     final TextEditingController passwordController = TextEditingController();
 
     void navigateBack() {
       Navigator.pop(context);
+    }
+
+    void handleSubmit() async {
+      if (_formKey.currentState.validate()) {
+        FirebaseAuth auth = FirebaseAuth.instance;
+        FirebaseFirestore firestore = FirebaseFirestore.instance;
+        CollectionReference users =
+            FirebaseFirestore.instance.collection('users');
+
+        try {
+          UserCredential userCredential = await FirebaseAuth.instance
+              .createUserWithEmailAndPassword(
+                  email: emailController.text,
+                  password: passwordController.text);
+          await users.doc(userCredential.user.uid).set({
+            "fullName": fullNameController.text,
+            "email": emailController.text
+          });
+
+          EasyLoading.showSuccess('User registered');
+        } catch (e) {
+          EasyLoading.showError(e.message);
+        }
+      }
     }
 
     return Scaffold(
@@ -18,118 +47,120 @@ class Register extends StatelessWidget {
         height: double.infinity,
         width: double.infinity,
         padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Register",
-              style: TextStyle(color: ThemeColors.white, fontSize: 26),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  TextFormField(
-                    style: TextStyle(color: ThemeColors.white),
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      hintText: 'Enter your full name',
-                      hintStyle: TextStyle(color: ThemeColors.white),
-                      filled: true,
-                      fillColor: ThemeColors.lightBlack,
-                    ),
-                    validator: (String value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    style: TextStyle(color: ThemeColors.white),
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      hintText: 'Enter your email',
-                      hintStyle: TextStyle(color: ThemeColors.white),
-                      filled: true,
-                      fillColor: ThemeColors.lightBlack,
-                    ),
-                    validator: (String value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    controller: passwordController,
-                    style: TextStyle(color: ThemeColors.white),
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      hintText: 'Enter your password',
-                      hintStyle: TextStyle(color: ThemeColors.white),
-                      filled: true,
-                      fillColor: ThemeColors.lightBlack,
-                    ),
-                    validator: (String value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState.validate()) {
-                          print("email " + emailController.text);
-                          print("password " + passwordController.text);
-                        }
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Text('Submit'),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  GestureDetector(
-                      onTap: navigateBack,
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                          "Back to login",
-                          textAlign: TextAlign.right,
-                          style: TextStyle(color: ThemeColors.white),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Register",
+                  style: TextStyle(color: ThemeColors.white, fontSize: 26),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      TextFormField(
+                        style: TextStyle(color: ThemeColors.white),
+                        controller: fullNameController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          hintText: 'Enter your full name',
+                          hintStyle: TextStyle(color: ThemeColors.white),
+                          filled: true,
+                          fillColor: ThemeColors.lightBlack,
                         ),
-                      ))
-                ],
-              ),
-            )
-          ],
+                        validator: (String value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        style: TextStyle(color: ThemeColors.white),
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          hintText: 'Enter your email',
+                          hintStyle: TextStyle(color: ThemeColors.white),
+                          filled: true,
+                          fillColor: ThemeColors.lightBlack,
+                        ),
+                        validator: (String value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        controller: passwordController,
+                        style: TextStyle(color: ThemeColors.white),
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          hintText: 'Enter your password',
+                          hintStyle: TextStyle(color: ThemeColors.white),
+                          filled: true,
+                          fillColor: ThemeColors.lightBlack,
+                        ),
+                        validator: (String value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: handleSubmit,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: Text('Submit'),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      GestureDetector(
+                          onTap: navigateBack,
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: Text(
+                              "Back to login",
+                              textAlign: TextAlign.right,
+                              style: TextStyle(color: ThemeColors.white),
+                            ),
+                          ))
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
